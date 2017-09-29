b8d0b34b34e05def4304495abb7abb490e883cee
Jorge A <ardonz@gmail.com>
2017-09-29 12:22:40 -0600

    testing

--- app.js
+++ app.js
@@ -8,27 +8,29 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 const favicon = require('serve-favicon');
 const FacebookStrategy = require('passport-facebook').Strategy;
+const app = express();
 let rooms = [];
-
-let app = express();
-let env = process.env.NODE_ENV || 'development';
-
-mongoose.connect(configOptions.dbUrl);
-mongoose.connection.on('connected', () => {
-    console.log('Connected to database');
-})
-mongoose.Promise = global.Promise;
-
-mongoose.connection.on('error', (err) => {
-    console.log('Database error: '+err);
-})
 
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('hogan-express'));
 app.set('view engine', 'html');
 
+mongoose.connect(configOptions.dbUrl);
+mongoose.connection.on('connected', () => {
+    console.log('Connected to database');
+})
+
+mongoose.connection.on('error', (err) => {
+    console.log('Database error: '+err);
+})
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
+
+app.use(favicon(__dirname + '/public/images/chaticon.ico'));
+
+let env = process.env.NODE_ENV || 'development';
+
 if(env === 'development') {
 	app.use(session({secret: configOptions.sessionSecret , saveUninitialized:true, resave:true}));
 } else {
@@ -42,7 +44,7 @@ if(env === 'development') {
 	}));
 }
 
-app.use(favicon(__dirname + '/public/images/chaticon.ico'));
+
 
 app.use(passport.initialize());
 app.use(passport.session());
